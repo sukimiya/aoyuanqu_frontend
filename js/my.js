ebe98123de0a21933b877eb77231d2235f1cce6c
sukimiya <sukimiya@github.com>
2017-05-12 16:54:26 +0800

    我的钱包 手机注册页面ajax

---
+++ js/my.js
@@ -0,0 +1,12 @@
+function GetRequest() {
+    var url = location.search;
+    var theReq = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        var strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theReq[strs[i].split("=")[0] = unescape(strs[i].split("=")[1])];
+        }
+    }
+    return theReq;
+}
