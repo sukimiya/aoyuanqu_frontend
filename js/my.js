8fcf70fbc92558e702edaf3d4925bdfb992535f8
周琳琳 <nina.xi@qq.com>
2017-06-07 22:33:03 +0800

    wxconfig

--- js/my.js
+++ js/my.js
@@ -239,7 +239,7 @@ var myweixin = (function () {
         debugger;
         wx.config({
             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: wxdata.appid, // 必填，公众号的唯一标识
+            appId: appid, // 必填，公众号的唯一标识
             timestamp: mytimestamp, // 必填，生成签名的时间戳
             nonceStr: mynonceStr, // 必填，生成签名的随机串
             signature: signatureSHA1, // 必填，签名，见附录1
